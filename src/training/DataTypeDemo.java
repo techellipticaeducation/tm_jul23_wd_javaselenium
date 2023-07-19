a6787b92a7089e7eed6d4eec4b76473774ba338e
Teche <techellipticaeducation@gmail.com>
2023-07-19 09:55:13 +0530

    data type - 1

---
+++ src/training/DataTypeDemo.java
@@ -0,0 +1,40 @@
+package training;
+
+public class DataTypeDemo {
+	
+	public static void main(String[] args) {
+		byte b1 = (byte)300;
+		System.out.println(b1);
+		
+		// 256
+		
+		// casting concept
+		// 
+	}
+	
+	// 1 byte = 8 bits
+	// 2 power 8 = value // 256
+	
+	//	256 /2 = 128
+	// 	-128 to 127
+	//  - (2 power totalbits) to [(2 power totalbits)-1]
+	// 	-128 to 127
+	// -2power7 to (2power7)-1
+ 	// -128 to 127
+	
+	
+	// short = 16 bit
+	// 2 power 16
+	// -2 power 15 to (2 power 15)-1
+	
+	
+	// int = 32 bit
+	// 2 power 32
+	// -2 power 31 to (2 power 31)-1 
+	
+	
+	
+	// long = 64 bit
+	// 2 power 64
+	// -2 power 63 to (2 power 63)-1
+}
