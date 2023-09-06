25c1257c86271a55ec6de45a34c7b0d034c24928
Teche <techellipticaeducation@gmail.com>
2023-09-05 22:30:37 -0500

    overloading

---
+++ src/training/MethodOverloadingDemo.java
@@ -0,0 +1,38 @@
+package training;
+
+public class MethodOverloadingDemo {
+	public static void main(String...args) {
+		int ret = CalculatorProject.add(10, 20);
+		System.out.println(ret);
+		ret = CalculatorProject.add(10, 20,30);
+		System.out.println(ret);
+		ret = CalculatorProject.add(10, 20,3,0,5,22,9,23,223);
+		System.out.println(ret);
+		ret = CalculatorProject.add(30);
+		System.out.println(ret);
+	}
+}
+
+
+class CalculatorProject{
+	public static int add(int k,int...i) {
+		int sum = 0;
+		for(int j = 0 ; j < i.length; j++) {
+			sum = sum + i[j];
+		}
+		return sum;
+	}
+
+	// Method Overloading - having same method name, but different number of param or different type of param
+	// return type will not make any impact
+	// Compile time polymorphism
+	//  method name is same but mathod
+	//  varargs = var (various) + args (arguments) = 
+
+	//  2 rules
+	// 1 - in one param list , only 1 varargs is possisble
+	// 2 - varargs must be the last param in param list
+
+
+
+}
--- src/utils/DateDemo.java
+++ src/utils/DateDemo.java
@@ -5,25 +5,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateDemo {
-public static void main(String[] args) {
-	Date dt = new Date();
-	System.out.println(dt);
-	
-	
-	// Format
-	// 22-Aug-2023     dd-MMM-yyyy
-	// 22-10-2023      dd-MM-yyyy
-	// 2023-10-22      yyyy-MM-dd
-	// 2023-22-10      yyyy-dd-MM
-	// 22-August-2023  
-	
-	SimpleDateFormat fb = new SimpleDateFormat("dd-MMM-yyyy");
-	String jk = fb.format(dt);
-	System.out.println(jk);
-	
-	File fl  = new File
-			("C:\\Users\\Vaibhav\\Desktop\\TechElliptica\\testometer_20sep\\dem\\"+jk);
-	fl.mkdir();
-	
-}
+	public static void main(String[] args) {
+		Date dt = new Date();
+		System.out.println(dt);
+
+
+		// Format
+		// 22-Aug-2023     dd-MMM-yyyy
+		// 22-10-2023      dd-MM-yyyy
+		// 2023-10-22      yyyy-MM-dd
+		// 2023-22-10      yyyy-dd-MM
+		// 22-August-2023  
+
+		SimpleDateFormat fb = new SimpleDateFormat("dd-MMM-yyyy");
+		String jk = fb.format(dt);
+		System.out.println(jk);
+
+		File fl  = new File
+				("C:\\Users\\Vaibhav\\Desktop\\TechElliptica\\testometer_20sep\\dem\\"+jk);
+		fl.mkdir();
+
+	}
 }
